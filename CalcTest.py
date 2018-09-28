df6869f9b5e005ae9f9eb78c280c57a19172267c
Zachery Koppert <zachery.koppert@tektronix.com>
2018-09-28 16:54:12 -0700

    Basic working add and subtract test
    Signed-off-by: Zachery Koppert <zachery.koppert@tektronix.com>

--- CalcTest.py
+++ CalcTest.py
@@ -1,11 +1,18 @@
 import unittest
+from Calculator import calculator
 
+class CalcTestDummie(unittest.TestCase):
 
+	def test_alwaysPass(self):
+		self.assertTrue(True)
 
-class CalcTestCase(unittest.TestCase)
-    calculator = Calculator()
-    def test_add(self):
-        self.assertEqual(4, self.calculator.add(2,2))
+class CalcTestOperations(unittest.TestCase):
+
+	def test_add(self):
+		self.assertEqual(calculator.add(2,2),4)
+
+	def test_subtract(self):
+		self.assertEqual(calculator.subtract(10,5),5)
 
 if __name__ == "__main__":
     unittest.main()
--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-test:
-	python -m unittest
+test_file = CalcTest
+
+test: Smoke Operations
+
+.PHONY: test Smoke Operations
+
+Smoke:
+	python3 -m unittest $(test_file).CalcTestDummie -v
+Operations:
+	python3 -m unittest $(test_file).CalcTestOperations -v
